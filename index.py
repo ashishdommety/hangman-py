490599c516eb11cce9986fb314f4e320fe0f6fb8
unknown <ashishuiux@gmail.com>
2018-10-18 14:01:49 -0700

    half-baked solution

--- index.py
+++ index.py
@@ -1,29 +1,25 @@
-# Get player 1 input
-print("Welcome to the hangman game!")
-key = input("User 1 goes first. Hide the screen from User 2 and enter a word:\n")
+def all_guesses(guesses_left):
+  print("Welcome to the hangman game!")
+  key = input("User 1 goes first. Hide the screen from User 2 and enter a word:\n")
+  print("\n" * 50)
+  answer = list("_" * len(key))
+  key = list(key)
+  print('\nHello Player 2!\n')
 
-# Clear Screen
-print("\n" * 50)
-
-# split the key into an array and display the number of blanks
-print("_ " * len(key))
+  while guesses_left != 0:
+    guess = input("You have "+ str(guesses_left) +" attempts left")
+    # Check if element exists in array
+    if (key.index(guess)):
+      print('you got it!\n')
+      print(answer[key.index(guess)])
+      answer[key.index(guess)] = guess
+      print("".join(answer))
+    else:
+      print('oops, wrong guess... try again\n')
+      guesses_left = guesses_left - 1 
   
-# Ask player 2 for their guess
-attempts = 5
-
-def guess(guesses_left):
-  print("You have "+ str(guesses_left) +" attempts left")
-  guess = input("Guess away!\n")
-  # Check if element exists in array
-  if key.index(guess) > -1:
-    print('you got it!')
-    for letter in key:
-      if letter == guess:
-        print(guess, end ="")
-      else:
-        print("_ ", end ="")
-  else:
-    guesses_left = guesses_left - 1 
+  print('You lose')
 
 # If the user guesses the word, display "You won! Player 2 wins!"
+all_guesses(5)
 # If the user runs out of attempts and the answer hasn't been guessed, respond with "You lose... Player 1 wins!"

64c5d05239ef340f363468983671fae5e1f64a1c
unknown <ashishuiux@gmail.com>
2018-10-18 14:26:46 -0700

    functionality for words with no recurring letters

--- index.py
+++ index.py
@@ -4,22 +4,25 @@ def all_guesses(guesses_left):
   print("\n" * 50)
   answer = list("_" * len(key))
   key = list(key)
+
   print('\nHello Player 2!\n')
 
   while guesses_left != 0:
     guess = input("You have "+ str(guesses_left) +" attempts left")
-    # Check if element exists in array
-    if (key.index(guess)):
+    # Check if element exists in key
+    if (guess in key):
       print('you got it!\n')
-      print(answer[key.index(guess)])
+      # print(answer[key.index(guess)])
       answer[key.index(guess)] = guess
+      if("_" not in answer):
+        print('you win!!!!!')
+        return
       print("".join(answer))
+    # if it doesn't, ask user to try again
     else:
       print('oops, wrong guess... try again\n')
       guesses_left = guesses_left - 1 
   
   print('You lose')
 
-# If the user guesses the word, display "You won! Player 2 wins!"
 all_guesses(5)
-# If the user runs out of attempts and the answer hasn't been guessed, respond with "You lose... Player 1 wins!"
